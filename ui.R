841ba5a26565a95c49ff6849a9f1e0eab28fb22c
ampsoma <ampsoma@gmail.com>
2015-01-27 10:12:58 -0700

    Inital commit, nothing works. This thing is a bundle of useless code. AAARRRGGG!!!

---
+++ ui.R
@@ -0,0 +1,40 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("AutoInovice"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("wage",
+                "Wage Per:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+      sliderInput("interval",
+                  "interval of wage",
+                  min=1,
+                  max=100,
+                  value=50)
+    dateRangeInput("dateRange",
+                 "Range of dates for work",
+                  start = Sys.Date()-7,
+                  end = Sys.Date()+1)
+    ),
+
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
