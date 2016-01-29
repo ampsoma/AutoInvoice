ad1b4db3b529ddd228f2239db91c89fae5ece64f
ampsoma <ampsoma@gmail.com>
2016-01-28 22:28:19 -0500

    NEW, BRAND SPKN NEW
    who knows, what this is I just need to Commit to this and begin working
    at another date. damn shiny server shit.

--- DB_Access.R
+++ DB_Access.R
@@ -1,8 +1,10 @@
 ## Trying out db access to Time Rec stuff
 library(RSQLite)
-
+# still have to manually un archive file .gz type
+data<-read.table(gzfile('/Users/samdougherty/Dropbox/TimeRecording/backup/timerec.db.gz'))
 con<-dbConnect(SQLite(), "/Users/samdougherty/Dropbox/TimeRecording/backup/timerec.db")
 dbBegin(con)
 dbListTables(con)
 
 dbReadTable(con,"T_category_1")
+
